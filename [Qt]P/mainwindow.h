5b354bb522084d5572b799cffc427168bc3558a1
xiangbin099 <494428152@qq.com>
2019-12-25 22:50:01 +0800

    add qt

---
+++ [Qt]P/mainwindow.h
@@ -0,0 +1,74 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QProcess>
+#include <QDebug>
+#include <QMessageBox>
+#include <QFile>
+#include <QFileDialog>
+#include <QDir>
+#include <QTextStream>
+#include <QtSerialPort/QSerialPort>
+#include <QtSerialPort/QSerialPortInfo>
+#include <QTimer>
+#include <QWidget>
+#include <QFont>
+#include <QVector>
+#include <QDebug>
+#include <QColorDialog>
+
+#include "serial_send_receive.h"
+
+#include <QThread>
+#include <inttypes.h>
+#include <string>
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+    void Update_process(void);
+    uint8_t data_prepare(void);
+    uint32_t CRC32Software( uint32_t *pData, uint16_t Length );
+
+private slots:
+    void System_Ticks();
+    void Receieve_Bytes(void);
+    void on_pushButton_clicked();
+    void on_pushButton_3_clicked();
+    void on_pushButton_2_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    QTimer         *timer;
+    QByteArray      requestData;
+    QByteArray      file_bin;
+    QSerialPort    *my_serialport;
+
+    serial_send_receive *data_send_receive;
+
+    uint64_t        system_tick;
+    uint8_t         system_state;
+    uint8_t         system_package;
+
+    uint8_t         update_send_receieve_finish;
+    uint8_t         system_data[4];
+    uint8_t         system_adress[3];
+    uint32_t        adress;
+
+    uint32_t        crc32_data[60000];
+    uint16_t        crc32_length;
+};
+
+
+
+
+#endif // MAINWINDOW_H
---
+++ [Qt]P/serial_send.h
@@ -0,0 +1,23 @@
+﻿#ifndef SERIAL_SEND_H
+#define SERIAL_SEND_H
+
+
+#include <QMainWindow>
+#include <inttypes.h>
+class serial_send
+{
+public:
+    serial_send();
+    uint16_t UpdateCRC16(uint16_t crcIn, uint8_t byte);
+    uint16_t Cal_CRC16(uint8_t *data, uint32_t size);
+    void Cal_CRC16_Buf(uint8_t *buf);
+    void serial_send_request(QByteArray &buftomcu);
+    void serial_send_open_lock(QByteArray &buftomcu);
+    void serial_send_update(QByteArray &buftomcu, uint8_t *adress, uint8_t *data);
+    void serial_send_finish(QByteArray &buftomcu, uint8_t *adress, uint8_t *data);
+    void serial_erase_flash(QByteArray &buftomcu);
+    void serial_send_info(QByteArray &buftomcu);
+
+};
+
+#endif
